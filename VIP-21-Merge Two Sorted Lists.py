e346ae8539305cd6d0c52507c31a33bb81f3a440
Jiang_Shan <tostayreal@163.com>
2018-02-11 20:31:41 +0800

    Create VIP-21-Merge Two Sorted Lists.py
    some basic code about linked list

---
+++ VIP-21-Merge Two Sorted Lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if not l1 and not l2:
+            return None
+        result = ListNode(0)
+        l = result
+        while l1 and l2:
+            if l1.val < l2.val:
+                l.next = l1
+                l1 = l1.next
+            else:
+                l.next = l2
+                l2 = l2.next
+            l = l.next
+        if l1:
+            l.next = l1
+        else:
+            l.next = l2
+        return result.next
