f6cb1b005cb9ccec8228cf7902d27692ff716a8b
Jiang_Shan <tostayreal@163.com>
2018-02-13 20:14:08 +0800

    Create 66-Plus One.py
    Pay attention to take consideration of special numbers, such as [9]-->[1, 0].

---
+++ 66-Plus One.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+                if i == 0:
+                    digits.insert(0, 1)
+            else:
+                digits[i] += 1
+                return digits
+        return digits
