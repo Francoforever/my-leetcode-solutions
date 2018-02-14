0cb940efb9ed422c633af6c1547b41fffe54d73e
Jiang_Shan <tostayreal@163.com>
2018-02-14 11:43:37 +0800

    Update 67-simple-Add Binary.py

--- 67-simple-Add Binary.py
+++ 67-simple-Add Binary.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def addBinary(self, a, b):
-        inta = int(a, 2)
-        intb = int(b, 2)
-        res = inta + intb
-        res_str = bin(res)
-        return res_str[2:]
+        num = int(a,2) + int(b,2)
+        return bin(num)[2:]
